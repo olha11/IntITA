152c0ba9f11d907596e1ad6157bb2670f162b838
alex <avitlauak@gmail.com>
2015-02-04 20:04:21 +0200

    adding mainmenu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -1,64 +1,117 @@
 <?php /* @var $this Controller */ ?>
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<meta name="language" content="en">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="language" content="en" />
 
 	<!-- blueprint CSS framework -->
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection">
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print">
-	
-	<!-- slider, jQuery -->
-	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.js"></script>
-	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/scripts/slider.js"></script>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/slider.css">
-    <!-- slider, jQuery -->
-
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/screen.css" media="screen, projection" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/print.css" media="print" />
 	<!--[if lt IE 8]>
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection">
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/ie.css" media="screen, projection" />
 	<![endif]-->
 
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css">
-
-	<title><?php echo CHtml::encode($this->pageTitle); ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/main.css" />
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/form.css" />
+    <!-- stylesheet and script for layouts -->
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css" />
+    <!-- stylesheet and script for layouts -->
+    	<!-- slider, jQuery -->
+	<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.js"></script>
+	<script type="text/javascript" src="<?php echo Yii::app()->request->baseUrl; ?>/protected/scripts/slider.js"></script>
+	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/slider.css">
+    <!-- slider, jQuery -->
+    
+	<title>IntITA</title>
 </head>
 
 <body>
 
 <div class="container" id="page">
 
-	<div id="header">
-		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
-	</div><!-- header -->
+	<div id="navigation">
+                <div id="logo_img">
+				    <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/intITA_logo.png"/>
+                </div>
+                <div>
+                    <ul>
+                        <li><a href="http://www.google.com"><font color="4b75a5">Курси</a></li>
+                        <li><a href="http://www.google.com"><font color="4b75a5">Розклад</a></li>
+                        <li><a href="http://www.google.com"><font color="4b75a5">Викладачі</a></li>
+                        <li><a href="http://www.google.com"><font color="4b75a5">Форум</a></li>
+                        <li><a href="http://www.google.com"><font color="4b75a5">Про нас</a></li>
+                    </ul>
+                </div>
+                <div id="line_small_off">
+                
+                </div>
+                <div id="line_top">
+                <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/toper.png"/>
+                </div>
+                <div id="enter_button">
+                       <a href="http://www.google.com"><img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/enter_button.png"/></a>
+                    
+               </div>
+			</div>
+<script>
+   var up_menu = document.getElementById('navigation');
+   var key = document.getElementById('enter_button');
+   var line = document.getElementById('line_top');
+   var line_small = document.getElementById('line_small_off');
+       window.onscroll = function() {
+            var pageY = window.pageYOffset || document.documentElement.scrollTop;
+        if (pageY >= key.offsetHeight) {
+            key.className = "fixed";
+            up_menu.className = "navigation";
+            line.className = "none";
+            line_small.className = "line_small"
+        } else {
+            key.className = "";
+            line.className = "";
+            line_small.className = "";
+            up_menu.className = "";
+          }
+      }
+                        
+</script>
+<?php echo $content; ?>
+<script>
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
-			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
-			),
-		)); ?>
-	</div><!-- mainmenu -->
-	<?php if(isset($this->breadcrumbs)):?>
-		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
-			'links'=>$this->breadcrumbs,
-		)); ?><!-- breadcrumbs -->
-	<?php endif?>
-
-	<?php echo $content; ?>
-
-	<div class="clear"></div>
-
-	<div id="footer">
-		Copyright &copy; <?php echo date('Y'); ?> by My Company.<br/>
-		All Rights Reserved.<br/>
-		<?php echo Yii::powered(); ?>
-	</div><!-- footer -->
+</script>
+		<div class="main">
+                <div id="footer">
+                    <div>
+                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/sotial.gif"/>
+                    </div>
+                    <div class="footer">
+                        <div>
+                            <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/intITA_logo.png" id="logo"/>
+                        </div>
+                        <div>
+                            <p>м. Вінниця, вул. Петлюри, 12а</br>
+                            АйТі академія "INTITA"</p>
+                        </div>
+                        <div>
+                            <ul>
+                                <li><a href="http://www.google.com"><font color="4b75a5">Курси</a></li>
+                                <li><a href="http://www.google.com"><font color="4b75a5">Розклад</a></li>
+                                <li><a href="http://www.google.com"><font color="4b75a5">Викладачі</a></li>
+                                <li><a href="http://www.google.com"><font color="4b75a5">Форум</a></li>
+                                <li><a href="http://www.google.com"><font color="4b75a5">Про нас</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="footer">
+						<a href="#"><img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/go_up.gif"/></a>
+                    </div>
+                    <div class="footer">
+                    </div>
+                </div><!-- footer -->
+            </div>
+            <hr style="margin-top: -2px; border-bottom: 0px;border-left: 0px;border-right: 0px;border-top: 2px solid #00ffff" />
+            <div style="background-color: white; z-index: 1;"></div>
+	
 
 </div><!-- page -->
 
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -9,10 +9,10 @@ $this->pageTitle=Yii::app()->name;
 <div class="slider-box">
   <div class="slider">
     
-    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/1.jpg"/> 
-    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/2.jpg"/> 
-    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/3.jpg"/>
-    <img src="<?php echo Yii::app()->request->baseUrl; ?>/images/4.jpg"/> 
+    <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/1.jpg"/>
+    <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/2.jpg"/>
+    <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/3.jpg"/>
+    <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/4.jpg"/>
 
   </div>
 </div>
