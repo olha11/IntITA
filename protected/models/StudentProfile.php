8d78ee55c8445d902fcea06ba29641a1fb314b26
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-02-15 20:11:11 +0200

    no message

---
+++ protected/models/StudentProfile.php
@@ -0,0 +1,142 @@
+<?php
+
+/**
+ * This is the model class for table "studentprofile".
+ *
+ * The followings are the available columns in table 'studentprofile':
+ * @property integer $id
+ * @property string $firstName
+ * @property string $secondName
+ * @property string $middleName
+ * @property string $login
+ * @property string $birthday
+ * @property string $education
+ * @property string $aboutMyself
+ * @property string $interests
+ * @property string $certificates
+ * @property string $phone
+ * @property string $email
+ * @property string $address
+ * @property string $note
+ * @property string $password
+ * @property string $password_repeat
+ */
+class StudentProfile extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'studentprofile';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+            array('firstName, secondName, middleName,login, birthday, education, email, password, password_repeat', 'required', 'message'=>'Будь ласка введіть {attribute}.'),
+			array('firstName, secondName, middleName, education, certificates, email, password, password_repeat', 'length', 'max'=>255),
+			array('birthday', 'length', 'max'=>11),
+            array('email', 'email', 'message'=>'Email не являється правильною {attribute} адресою'),
+            array('birthday', 'date','format' => 'dd.MM.yyyy'),
+			array('phone', 'length', 'max'=>15),
+			array('aboutMyself, interests, address, note', 'safe'),
+            array('password', 'compare', 'compareAttribute'=>'password_repeat', 'message'=>'Паролі не співпадають'),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id, firstName, secondName, middleName, birthday, education, aboutMyself, interests, certificates, phone, email, address, note, password, password_repeat', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id' => 'ID',
+			'firstName' => 'Ім\'я',
+			'secondName' => 'Прізвище',
+			'middleName' => 'По-батькові',
+            'login' => 'Логин',
+			'birthday' => 'День народження',
+			'education' => 'Освіта',
+			'aboutMyself' => 'Про себе',
+			'interests' => 'Інтереси',
+			'certificates' => 'Сертифікати',
+			'phone' => 'Телефон',
+			'email' => 'Email',
+			'address' => 'Адреса',
+			'note' => 'Примітки',
+			'password' => 'Пароль',
+			'password_repeat' => 'Повтор пароля',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id',$this->id);
+		$criteria->compare('firstName',$this->firstName,true);
+		$criteria->compare('secondName',$this->secondName,true);
+		$criteria->compare('middleName',$this->middleName,true);
+        $criteria->compare('login',$this->birthday,true);
+		$criteria->compare('birthday',$this->birthday,true);
+		$criteria->compare('education',$this->education,true);
+		$criteria->compare('aboutMyself',$this->aboutMyself,true);
+		$criteria->compare('interests',$this->interests,true);
+		$criteria->compare('certificates',$this->certificates,true);
+		$criteria->compare('phone',$this->phone,true);
+		$criteria->compare('email',$this->email,true);
+		$criteria->compare('address',$this->address,true);
+		$criteria->compare('note',$this->note,true);
+		$criteria->compare('password',$this->password,true);
+		$criteria->compare('password_repeat',$this->password_repeat,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return StudentProfile the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+}
---
+++ protected/views/studentprofile/studentprofile.php
@@ -0,0 +1,138 @@
+<?php
+/* @var $this StudentProfileController */
+/* @var $model StudentProfile */
+/* @var $form CActiveForm */
+?>
+
+<?php
+/* @var $this StudentprofileController */
+/* @var $model Studentprofile */
+/* @var $form CActiveForm */
+?>
+
+<div class="formStudProf">
+
+    <div class="studProf">
+        <?php $form=$this->beginWidget('CActiveForm', array(
+            'id'=>'studentprofile-form',
+            // Please note: When you enable ajax validation, make sure the corresponding
+            // controller action is handling ajax validation correctly.
+            // There is a call to performAjaxValidation() commented in generated controller code.
+            // See class documentation of CActiveForm for details on this.
+            'enableAjaxValidation'=>false,
+        )); ?>
+        <legend class="titleProfile">
+               <h3>Персональні дані</h3> 
+        </legend>
+
+            <div class="row">
+               <?php echo $form->label($model,'firstName'); ?>
+                <?php echo $form->textField($model,'firstName',array('maxlength'=>255)); ?>
+               <span><?php echo $form->error($model,'firstName'); ?></span>
+            </div>
+
+            <div class="row">
+                <?php echo $form->label($model,'secondName'); ?>
+                <?php echo $form->textField($model,'secondName',array('maxlength'=>255)); ?>
+               <span><?php echo $form->error($model,'secondName'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->label($model,'middleName'); ?>
+                <?php echo $form->textField($model,'middleName',array('maxlength'=>255)); ?>
+               <span><?php echo $form->error($model,'middleName'); ?></span>
+            </div>
+            
+            <div class="row">
+                <?php echo $form->label($model,'login'); ?>
+                <?php echo $form->textField($model,'login',array('maxlength'=>255)); ?>
+               <span><?php echo $form->error($model,'login'); ?></span>
+            </div>
+            
+            <div class="row">
+                <?php echo $form->label($model,'birthday'); ?>
+                <?php echo $form->textField($model,'birthday',array('maxlength'=>11)); ?>
+               <span><?php echo $form->error($model,'birthday'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->label($model,'education'); ?>
+                <?php echo $form->textField($model,'education',array('maxlength'=>255)); ?>
+               <span><?php echo $form->error($model,'education'); ?></span>
+            </div>
+        
+            <div class="row">
+              <?php echo $form->labelEx($model,'aboutMyself'); ?>
+               <?php echo $form->textArea($model,'aboutMyself'); ?>
+               <span><?php echo $form->error($model,'aboutMyself'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->labelEx($model,'interests'); ?>
+                <?php echo $form->textArea($model,'interests',array('rows'=>5)); ?>
+               <span><?php echo $form->error($model,'interests'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->labelEx($model,'certificates'); ?>
+                <?php echo $form->textField($model,'certificates',array('maxlength'=>255)); ?>
+               <span><?php echo $form->error($model,'certificates'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->labelEx($model,'phone'); ?>
+                <?php echo $form->textField($model,'phone',array('maxlength'=>15)); ?>
+               <span><?php echo $form->error($model,'phone'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->label($model,'email'); ?>
+                <?php echo $form->textField($model,'email',array('maxlength'=>255)); ?>
+               <span><?php echo $form->error($model,'email'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->labelEx($model,'address'); ?>
+                <?php echo $form->textArea($model,'address'); ?>
+               <span><?php echo $form->error($model,'address'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->labelEx($model,'note'); ?>
+                <?php echo $form->textArea($model,'note'); ?>
+               <span><?php echo $form->error($model,'note'); ?></span>
+            </div>
+
+            <div class="rowPass">
+                <?php echo $form->label($model,'password'); ?>
+                <?php echo $form->passwordField($model,'password',array('maxlength'=>255)); ?>
+               <span class="passEye"><?php echo $form->error($model,'password'); ?></span>
+            </div>
+        
+            <div class="row">
+                <?php echo $form->label($model,'password_repeat'); ?>
+                <?php echo $form->passwordField($model,'password_repeat',array('maxlength'=>255)); ?>
+                <span class="passEye"><?php echo $form->error($model,'password_repeat'); ?></span>
+            </div>
+            <div class="row buttons">
+                <?php echo CHtml::submitButton('ВІДПРАВИТИ />', array('id' => "submit")); ?>
+            </div>
+        <?php $this->endWidget(); ?>
+    </div>
+    <div class="studPhoto">
+        <legend class="titleProfile">
+               <h3>Завантажити фото профілю</h3> 
+        </legend>
+        <img src="<?php echo Yii::app()->request->baseUrl; ?>/image/1id.jpg"/>
+        <form action="/index.php?r=studentprofile/uploadAvatar" method="post" enctype="multipart/form-data">
+        <div class="uploadAvatar">
+             <input type="file" name="filename"></br> 
+        </div>     
+             <label class="avatarInfo">  Файл не вибрано... </label></br> 
+            <input type="submit" value="ЗАВАНТАЖИТИ"></br>
+        </form>
+    </div>
+	
+</div><!-- form -->
+
+
