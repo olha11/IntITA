4eb177f2c1bdc1197b0bbfe6fa0f497499632556
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-02-25 18:51:33 +0200

    Step update, profile-reg update

--- protected/views/studentreg/studentreg.php
+++ protected/views/studentreg/studentreg.php
@@ -51,7 +51,7 @@
         </div>
 
         <div class="row">
-            <?php echo $form->label($model,'firstName'); ?>
+            <?php echo $form->labelEx($model,'firstName'); ?>
             <?php echo $form->textField($model,'firstName',array('maxlength'=>255)); ?>
             <span><?php echo $form->error($model,'firstName'); ?></span>
         </div>
@@ -61,82 +61,68 @@
             <?php echo $form->textField($model,'secondName',array('maxlength'=>255)); ?>
             <span><?php echo $form->error($model,'secondName'); ?></span>
         </div>
-
         <div class="row">
-            <?php echo $form->label($model,'middleName'); ?>
-            <?php echo $form->textField($model,'middleName',array('maxlength'=>255)); ?>
-            <span><?php echo $form->error($model,'middleName'); ?></span>
+            <?php echo $form->labelEx($model,'nickname'); ?>
+            <?php echo $form->textField($model,'nickname',array('size'=>60,'maxlength'=>255)); ?>
+            <span><?php echo $form->error($model,'nickname'); ?></span>
         </div>
-
-        <div class="row">
-            <?php echo $form->label($model,'login'); ?>
-            <?php echo $form->textField($model,'login',array('maxlength'=>255)); ?>
-            <span><?php echo $form->error($model,'login'); ?></span>
-        </div>
-
         <div class="row">
             <?php echo $form->label($model,'birthday'); ?>
             <?php echo $form->textField($model,'birthday',array('maxlength'=>11)); ?>
             <span><?php echo $form->error($model,'birthday'); ?></span>
         </div>
-
         <div class="row">
-            <?php echo $form->label($model,'education'); ?>
-            <?php echo $form->textField($model,'education',array('maxlength'=>255)); ?>
-            <span><?php echo $form->error($model,'education'); ?></span>
+            <?php echo $form->labelEx($model,'email'); ?>
+            <?php echo $form->textField($model,'email',array('maxlength'=>255)); ?>
+            <span><?php echo $form->error($model,'email'); ?></span>
         </div>
-
         <div class="row">
-            <?php echo $form->labelEx($model,'aboutMyself'); ?>
-            <?php echo $form->textArea($model,'aboutMyself'); ?>
-            <span><?php echo $form->error($model,'aboutMyself'); ?></span>
+            <?php echo $form->labelEx($model,'password'); ?>
+            <span class="passEye"><?php echo $form->passwordField($model,'password',array('maxlength'=>255)); ?></span>
+            <?php echo $form->error($model,'password'); ?>
         </div>
-
-        <div class="row">
-            <?php echo $form->labelEx($model,'interests'); ?>
-            <?php echo $form->textArea($model,'interests',array('rows'=>5)); ?>
-            <span><?php echo $form->error($model,'interests'); ?></span>
+        <div class="rowPass">
+            <?php echo $form->labelEx($model,'password_repeat'); ?>
+            <span class="passEye">  <?php echo $form->passwordField($model,'password_repeat',array('maxlength'=>255)); ?></span>
+            <?php echo $form->error($model,'password_repeat'); ?>
         </div>
-
-        <div class="row">
-            <?php echo $form->labelEx($model,'certificates'); ?>
-            <?php echo $form->textField($model,'certificates',array('maxlength'=>255)); ?>
-            <span><?php echo $form->error($model,'certificates'); ?></span>
-        </div>
-
         <div class="row">
             <?php echo $form->labelEx($model,'phone'); ?>
             <?php echo $form->textField($model,'phone',array('maxlength'=>15)); ?>
             <span><?php echo $form->error($model,'phone'); ?></span>
         </div>
-
-        <div class="row">
-            <?php echo $form->label($model,'email'); ?>
-            <?php echo $form->textField($model,'email',array('maxlength'=>255)); ?>
-            <span><?php echo $form->error($model,'email'); ?></span>
-        </div>
-
         <div class="row">
             <?php echo $form->labelEx($model,'address'); ?>
-            <?php echo $form->textArea($model,'address'); ?>
+            <?php echo $form->textField($model,'address'); ?>
             <span><?php echo $form->error($model,'address'); ?></span>
         </div>
-
         <div class="row">
-            <?php echo $form->labelEx($model,'note'); ?>
-            <?php echo $form->textArea($model,'note'); ?>
-            <span><?php echo $form->error($model,'note'); ?></span>
-        </div>
-
-        <div class="rowPass">
-            <?php echo $form->label($model,'password'); ?>
-            <span class="passEye"><?php echo $form->passwordField($model,'password',array('maxlength'=>255)); ?></span>
-            <?php echo $form->error($model,'password'); ?>
+            <?php echo $form->label($model,'education'); ?>
+            <?php echo $form->textField($model,'education',array('maxlength'=>255)); ?>
+            <span><?php echo $form->error($model,'education'); ?></span>
         </div>
         <div class="row">
-            <?php echo $form->label($model,'password_repeat'); ?>
-            <span class="passEye">  <?php echo $form->passwordField($model,'password_repeat',array('maxlength'=>255)); ?></span>
-            <?php echo $form->error($model,'password_repeat'); ?>
+            <?php echo $form->labelEx($model,'educform'); ?>
+            <div class="rowRadioButton">
+                <?php $model->educform = '1'; ?>
+                 <?php echo $form->radioButtonList($model,'educform',array('1'=>'online','0'=>'offline'),array('separator'=>'',)); ?>
+                <span><?php echo $form->error($model,'educform'); ?></span>
+            </div>
+        </div>
+        <div class="row">
+            <?php echo $form->labelEx($model,'interests'); ?>
+            <?php echo $form->textField($model,'interests',array('rows'=>5)); ?>
+            <span><?php echo $form->error($model,'interests'); ?></span>
+        </div>
+        <div class="row">
+            <?php echo $form->labelEx($model,'aboutUs'); ?>
+            <?php echo $form->textField($model,'aboutUs',array('rows'=>6, 'cols'=>50)); ?>
+            <span> <?php echo $form->error($model,'aboutUs'); ?></span>
+        </div>
+        <div class="row">
+            <?php echo $form->labelEx($model,'send_letter'); ?>
+            <?php echo $form->textArea($model,'send_letter',array('rows'=>6, 'cols'=>50)); ?>
+            <span> <?php echo $form->error($model,'aboutUs'); ?></span>
         </div>
         <div class="row buttons">
             <?php echo CHtml::submitButton('ВІДПРАВИТИ />', array('id' => "submit")); ?>
