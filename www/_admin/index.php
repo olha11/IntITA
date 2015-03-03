74f95627e01d19d45af2c1499bbd5d99b49dff6d
alex <avitlauak@gmail.com>
2015-03-03 16:24:15 +0200

    change index path

--- www/_admin/index.php
+++ www/_admin/index.php
@@ -1,9 +1,7 @@
 <?php
-define('YII_DIR', 'yii');
-define('YII_PROTECTED', 'protected');
 
-$yii=dirname(__FILE__).'/../../../'.YII_DIR.'/framework/yii.php';	//Path to Yii distributive
-$config=dirname(__FILE__).'/../../'.YII_PROTECTED.'/config/autoadmin.local.php';	//Path to back-end config
+$yii=dirname(__FILE__).'/../../protected/framework/yii.php';	//Path to Yii distributive
+$config=dirname(__FILE__).'/../../protected/config/autoadmin.local.php';	//Path to back-end config
 
 require_once($yii);
 Yii::createWebApplication($config)->run();
