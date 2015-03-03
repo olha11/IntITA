0c47c7d69ad04e281fc5901bf218b8f557a674e1
IvannaBezpalko <vnnchkh@gmail.com>
2015-03-03 16:17:55 +0200

    update db

---
+++ protected/modules/autoAdmin/views/default/index.php
@@ -0,0 +1,18 @@
+<?php
+$this->pageTitle = 'Back-end index page';
+
+$this->breadcrumbs=array(
+	$this->pageTitle
+);
+?>
+<h1>AutoAdmin CMS FrameWork</h1>
+
+<h2>Розділи</h2>
+<ul>
+	<li><a href="./mainpage/mainpageGeneral/">Головна сторінка - заголовки</a></li>
+	<li><a href="./mainpage/carousel/">Головна сторінка - слайдер</a></li>
+	<li><a href="./mainpage/blocks/">Головна сторінка - блоки</a></li>
+	<li><a href="./steps/stepslist/">Головна сторінка - кроки</a></li>
+</ul>
+
+<br clear="both"/>
