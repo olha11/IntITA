c44fc109a3af4876015a4aee75a63c35b52dca04
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-03-10 17:50:28 +0200

    10 stars

--- protected/views/teachers/teachers.php
+++ protected/views/teachers/teachers.php
@@ -62,13 +62,21 @@ $post=TeachersTemp::model()->findAll();
                                     ?>
                                 </ul>
                             </div>
-                            <div class="aboutMore">
-                                <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/readMore.png"/> <a href="<?php echo Yii::app()->request->baseUrl.$teacherValue->readMoreLink; ?>">Читать полностью &#187;</a></br>
-                                <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/starFull.png"/><a href="#">Отзывы о преподавателе &#187;</a>
-                            </div>
                         </td>
                     </tr>
                 </table>
+                <div class="aboutMore">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/readMore.png"/> <a href="<?php echo Yii::app()->request->baseUrl.$teacherValue->readMoreLink; ?>">Читать полностью &#187;</a></br>
+                    <?php
+                    for ($k=0; $k<10; $k++)
+                    {
+                        ?>
+                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/starFull.png"/>
+                    <?php
+                    }
+                    ?>
+                    <a href="#">Отзывы о преподавателе &#187;</a>
+                </div>
             </div>
             <?php
         }
@@ -111,13 +119,21 @@ $post=TeachersTemp::model()->findAll();
                                     ?>
                                 </ul>
                             </div>
-                            <div class="aboutMore">
-                                <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/readMore.png"/> <a href="#">Читать полностью &#187;</a></br>
-                                <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/starFull.png"/><a href="#">Отзывы о преподавателе &#187;</a>
-                            </div>
                         </td>
                     </tr>
                 </table>
+                <div class="aboutMore">
+                    <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/readMore.png"/> <a href="<?php echo Yii::app()->request->baseUrl.$teacherValue->readMoreLink; ?>">Читать полностью &#187;</a></br>
+                    <?php
+                    for ($k=0; $k<10; $k++)
+                    {
+                        ?>
+                        <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/starFull.png"/>
+                    <?php
+                    }
+                    ?>
+                    <a href="#">Отзывы о преподавателе &#187;</a>
+                </div>
             </div>
             <?php
         }
