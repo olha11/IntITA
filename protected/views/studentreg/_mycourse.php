40ad72f35656158d4bb6f5ab027c82baf1fd1f5d
Игорь Баранюк <Wizlightdragon@gmail.com>
2015-04-14 15:42:12 +0300

    tabs from cabinet

---
+++ protected/views/studentreg/_mycourse.php
@@ -0,0 +1,53 @@
+<p class="tabHeader">Мої курси</p>
+<div class="profileCourse">
+    <table>
+        <tr>
+            <td>
+                <p style="margin-left: 35px">Поточний курс:</p>
+                <span class="colorP">"Апгрейд головного мозку" </span>
+            </td>
+            <td>
+                <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/actualcourse.png"/>
+
+            </td>
+        </tr>
+    </table>
+    <p class="courseLevLang">Рівень курсу: <span class="colorP">сильний початківець</span></p>
+    <p class="courseLevLang">Мова курсу: <span class="colorP">українська</span></p>
+</div>
+<div class="profileModule">
+    <p>Модуль:</br> <span class="colorP">Модуль 4. Модульне око, модульний ніс</span></p>
+</div>
+<div class="profileLesson">
+    <p>Заняття:</br> <span class="colorP">Заняття 6. Повторіння мать учіння</span></p>
+</div>
+<div style="border-bottom:none" class="profileCourse">
+    <table>
+        <tr>
+            <td>
+                <p style="margin-left: 35px">Незавершений курс:</p>
+                <span class="colorP">"Як прокачати ельфа до 81-го рівня" </span>
+            </td>
+            <td>
+                <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/incompletecourse.png"/>
+            </td>
+        </tr>
+    </table>
+    <div class="studPay">
+        <p>Необхідно здійснити наступну проплату до 30.12.2015</p>
+        <p>Сума проплати: 1000грн</p>
+    </div>
+</div>
+<div style="border-bottom:none" class="profileCourse">
+    <table>
+        <tr>
+            <td>
+                <p style="margin-left: 35px">Завершений курс:</p>
+                <span class="colorP">"Відкрий у собі гобліна"</span>
+            </td>
+            <td>
+                <img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/completecourse.png"/>
+            </td>
+        </tr>
+    </table>
+</div>
