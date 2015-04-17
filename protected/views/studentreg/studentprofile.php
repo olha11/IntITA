d4f3230af0caf843d6bd5cf49b06167fb29e09d1
Serhiy Kmet <kmetsmediator@gmail.com>
2015-04-17 18:05:59 +0300

    studentprofile: page myRatting and finances

--- protected/views/studentreg/studentprofile.php
+++ protected/views/studentreg/studentprofile.php
@@ -1,5 +1,5 @@
 <!-- studprofile style -->
-<link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/studProfile.css" />
+<link type="text/css" rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/profile.css" />
 <!-- studprofile style -->
 <?php
 /* @var $this StudentprofileController */
@@ -7,24 +7,13 @@
 /* @var $form CActiveForm */
 ?>
 <?php
+$this->pageTitle = 'INTITA';
 $post=StudentReg::model()->findByPk(Yii::app()->user->id);
 ?>
-<div class="formStudProfNav">
-    <?php
-    $this->breadcrumbs=array(
-        Yii::t('breadcrumbs', 'Profile'),
-    );
-    ?>
-    <div class="profileStatus">
-        <div>
-            <?php echo $post->firstName;?></br>
-            <?php echo $post->secondName;?></br>
-            <?php echo $post->nickname;?></br>
-            <span style="color: #33cc00; font-size: smaller">&#x25A0; online</span>
-        </div>
-        <img src="<?php echo Yii::app()->request->baseUrl.$post->avatar; ?>"/>
-    </div>
-</div>
+<?php
+$this->breadcrumbs=array(Yii::t('breadcrumbs', 'Profile'),
+);
+?>
 <div class="formStudProf">
     <div class="studProfInf">
         <table class="titleProfile">
@@ -48,15 +37,17 @@ $post=StudentReg::model()->findByPk(Yii::app()->user->id);
                     <h1><?php echo $post->firstName;?></h1>
                     <h1><?php echo $post->secondName;?></h1>
                     <div class="aboutInfo">
-                        <p><?php echo $post->address;?>,
-                            <?php
+                        <p><?php echo $post->address;
+                            if(!empty($post->address)&&!empty($post->birthday)) echo ', ';
                             $myAge = $post->birthday;
                             $myAge = str_replace("/",".",$myAge);
                             $date_a = new DateTime($myAge);
                             $date_b = new DateTime();
                             $interval = $date_b->diff($date_a);
-                            echo $interval->format("%Y");
-                            $post::getYearsTermination($interval->format("%Y"));
+                            if($interval->format("%Y")!=='00'){
+                                echo $interval->format("%Y");
+                                $post::getYearsTermination($interval->format("%Y"));
+                            }
                             ?>
                         </p>
                     </div>
@@ -105,72 +96,49 @@ $post=StudentReg::model()->findByPk(Yii::app()->user->id);
         <div class="tabs">
             <input id="tab1" type="radio" name="tabs" checked>
             <label class="tabsUp1" for="tab1" title="Мої курси">Мої курси</label>
-            <input id="tab2" type="radio" name="tabs">
+            <input id="tab2" type="radio" name="tabs" >
             <label for="tab2" title="Розклад">Розклад</label>
-            <input id="tab3" type="radio" name="tabs">
+            <input id="tab3" type="radio" name="tabs" >
             <label for="tab3" title="Консультації">Консультації</label>
             <input id="tab4" type="radio" name="tabs">
             <label for="tab4" title="Екзамени">Екзамени</label>
             <input id="tab5" type="radio" name="tabs">
-            <label for="tab5" title="Мій рейтинг">Мій рейтинг</label>
+            <label for="tab5" title="Мої проекти">Проекти</label>
             <div class="lineUnderTab"></div>
             <input id="tab6" type="radio" name="tabs">
-            <label class="tabsDown1" for="tab6" title="Завантаження">Завантаження</label>
+            <label class="tabsDown1" for="tab6" title="Мій рейтинг">Мій рейтинг</label>
             <input id="tab7" type="radio" name="tabs">
-            <label class="tabsDown" for="tab7" title="Листування">Листування</label>
+            <label for="tab7" title="Завантаження">Завантаження</label>
             <input id="tab8" type="radio" name="tabs">
-            <label class="tabsDown" for="tab8" title="Мої оцінювання">Мої оцінювання</label>
+            <label class="tabsDown" for="tab8" title="Листування">Листування</label>
             <input id="tab9" type="radio" name="tabs">
-            <label class="tabsDown" style="background-image:url(<?php echo Yii::app()->request->baseUrl; ?>/css/images/financeico.png);background-repeat: no-repeat;background-position:10px 3px;" for="tab9" title="Фінанси">
-                Фінанси
-            </label>
+            <label class="tabsDown" for="tab9" title="Мої оцінювання">Мої оцінювання</label>
+            <input id="tab10" type="radio" name="tabs">
+            <label class="tabsDown" style="background-image:url(<?php echo Yii::app()->request->baseUrl; ?>/css/images/financeico.png);background-repeat: no-repeat;background-position:10px 3px;" for="tab10" title="Фінанси">Фінанси</label>
             <div class="lineUnderTab"></div>
             <section id="myCourse">
-                <div class="profileCourse">
-                    <p>Курс:</br> <span class="colorP">"Апгрейд головного мозку"</span></p>
-                    <p class="courseLevLang">Рівень курсу: <span class="colorP">сильний початківець</span></p>
-                    <p class="courseLevLang">Мова курсу: <span class="colorP">українська</span></p>
-                </div>
-                <div class="profileModule">
-                    <p>Модуль:</br> <span class="colorP">Модуль 4. Модульне око, модульний ніс</span></p>
-                </div>
-                <div class="profileLesson">
-                    <p>Заняття:</br> <span class="colorP">Заняття 6. Повторіння мать учіння</span></p>
-                </div>
-                <div class="profileCoursePay">
-                    <p>Курс:</br> <span class="colorP">"Як прокачати ельфа до 81-го рівня"</span></p>
-                    <div class="studPay">
-                        <p>Необхідно здійснити наступну проплату до 30.12.2015</p>
-                        <p>Сума проплати: 1000грн</p>
-                    </div>
-                </div>
+                <?php $this->renderPartial('_mycourse'); ?>
             </section>
             <section id="timetable">
-                <p>
-                    Тут буде розклад
-                </p>
+                <?php $this->renderPartial('_timetable'); ?>
             </section>
-            <section id="advice">
-                <p>
-                    Консультації
-                </p>
+            <section id="consultation">
+                <?php $this->renderPartial('_consultation'); ?>
             </section>
             <section id="exams">
-                <p>
-                    Екзамени
-                </p>
+                <?php $this->renderPartial('_exams'); ?>
+            </section>
+            <section id="projects">
+                <?php $this->renderPartial('_projects'); ?>
             </section>
             <section id="myRatting">
-                <p>
-                    Мій рейтинг
-                </p>
+                <?php $this->renderPartial('_myRatting'); ?>
             </section>
             <section id="myDownload">
-                <p>
-                    Якісь завантаження
-                </p>
+                <?php $this->renderPartial('_myDownload'); ?>
             </section>
             <section id="mylettersSend">
+                <?php $this->renderPartial('_mylettersSend'); ?>
                 <form method="post" action="<?php echo Yii::app()->request->baseUrl; ?>/?r=studentreg/sendletter">
                     <label for="letterTheme">Тема:</label>
                     <input type="text" id="letterTheme" name="letterTheme" /></br> </br>
@@ -183,14 +151,10 @@ $post=StudentReg::model()->findByPk(Yii::app()->user->id);
                 endif; ?>
             </section>
             <section id="myMark">
-                <p>
-                    Як я оцінюю
-                </p>
+                <?php $this->renderPartial('_myMark'); ?>
             </section>
             <section id="finances">
-                <p>
-                    Фінанси
-                </p>
+                <?php $this->renderPartial('_finances'); ?>
             </section>
         </div>
     </div>
