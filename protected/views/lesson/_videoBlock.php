0dfbcac805e6b40c259f65b488b61a5c49659e1a
Olha Shenyavska <olha11@ua.fm>
2015-04-18 01:44:33 +0300

    button download video

--- protected/views/lesson/_videoBlock.php
+++ protected/views/lesson/_videoBlock.php
@@ -5,4 +5,7 @@
  * Date: 14.04.2015
  * Time: 18:40
  */
-echo $data;
+echo $data;  ?>
+<div class="download" id="do2"> <a  href=""><img src="<?php echo Yii::app()->request->baseUrl; ?>/css/images/000zav-se-vid.png">Завантажити це відео</a></div>
+
+
