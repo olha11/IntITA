513df1d18007e0dd0986fcb71d9f8b3e86cccf00
IvannaBezpalko <vnnchkh@gmail.com>
2015-04-22 15:38:17 +0300

    Add table 'user' to dump.

--- int_ita_db/translatedmessagesen.sql
+++ int_ita_db/translatedmessagesen.sql
@@ -3,7 +3,7 @@
 -- Server version:               5.6.21 - MySQL Community Server (GPL)
 -- Server OS:                    Win32
 -- HeidiSQL version:             7.0.0.4053
--- Date/time:                    2015-04-22 09:15:01
+-- Date/time:                    2015-04-22 15:37:34
 -- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
